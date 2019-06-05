290abb5e2384fc560fb35b693b96b379534d4e85
shaktigurung <mr.shakti.gurung@gmail.com>
2019-06-05 13:11:17 +1000

    serverless setup

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './../styles/App.css';
+import CreatePost from './../components/createPost';
+import DisplayPosts from './../components/displayPosts';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Serverless Backend </h1> 
+      <CreatePost />
+      <DisplayPosts />
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './components/App';
+import AWSAppSyncClient, { AUTH_TYPE } from 'aws-appsync';
+import aws_config from './aws-exports';
+import { ApolloProvider } from 'react-apollo'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new AWSAppSyncClient({
+    url: aws_config.aws_appsync_graphqlEndpoint,
+    region: aws_config.aws_appsync_region,
+    auth: {
+        type: AUTH_TYPE.API_KEY,
+        apiKey: aws_config.aws_appsync_apiKey,
+    }
+});
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<ApolloProvider client={client}>
+    <App />
+</ApolloProvider>, document.getElementById('root'));
